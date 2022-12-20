3606b7204d44c055a13762a542b224b497daf226
alhajideen <salifunurudeen834@gmail.com>
2022-12-20 12:04:35 +0000

    add super constructor from Person and the play hooky method

--- student.rb
+++ student.rb
@@ -1,5 +1,11 @@
 require_relative './person'
 class Student < Person
-  def initialize()
-    
+  def initialize(age,classroom,name="unknown",parent_permission=true)
+    super(age,name,parent_permission)
+    @classroom=classroom
   end
+
+  def play_hooky
+    return "¯\(ツ)/¯"
+  end
+end
