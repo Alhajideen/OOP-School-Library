fd927917493bb4bc0abba594b31a774b0caf8aed
alhajideen <salifunurudeen834@gmail.com>
2022-12-20 12:23:09 +0000

    add can_use_services method in Student class

--- teacher.rb
+++ teacher.rb
@@ -1,3 +1,11 @@
+require_relative './person'
 class Person
-  
+  def initialize(age,specialization,name="unknown",parent_permission=true)
+    super(age,name,parent_permission)
+    @specialization=specialization
+  end
+
+  def can_use_services?
+    return true
+  end
 end
