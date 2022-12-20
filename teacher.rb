abe3babab9f2cd2e4344eda38be691cfa63d234e
alhajideen <salifunurudeen834@gmail.com>
2022-12-20 12:24:21 +0000

    refactor method to always return true

--- teacher.rb
+++ teacher.rb
@@ -1,5 +1,5 @@
 require_relative './person'
-class Person
+class Teacher < Person
   def initialize(age,specialization,name="unknown",parent_permission=true)
     super(age,name,parent_permission)
     @specialization=specialization
