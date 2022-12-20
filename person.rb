88fa0954300eba221210513aa13e1d72d09b46db
alhajideen <salifunurudeen834@gmail.com>
2022-12-20 11:29:26 +0000

    add class Person and a constructor

--- person.rb
+++ person.rb
@@ -1,5 +1,10 @@
 class Person
-      def method_name
-        
-      end
+  attr_accessor :name, :age
+  attr_reader :id
+  def initialize(age,name="unknown",parent_permission=true)
+    @id= Random.rand(1..50)
+    @age=age
+    @name=name
+    @parent_permission=parent_permission
+  end
 end
