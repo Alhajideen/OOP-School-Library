0aa399bcba890d468dd8e3fb72b05d5ef627dd0a
alhajideen <salifunurudeen834@gmail.com>
2022-12-21 19:23:46 +0000

    add a test_decorator.rb file

--- trimmer_decorator.rb
+++ trimmer_decorator.rb
@@ -1,5 +1,5 @@
 require_relative 'decorator.rb'
-class ClassName < Nameable
+class TrimmerDecorator < Nameable
   def correct_name
     value = @nameable.correct_name
     value.length > 10 ? "#{value[0...10]}" : value
