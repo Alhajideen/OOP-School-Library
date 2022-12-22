40d15823bf2cdf95639b5fc23f84ed9d6278318b
alhajideen <salifunurudeen834@gmail.com>
2022-12-22 22:30:04 +0000

    add the has-many and belong-to relationship for book and rentals

--- book.rb
+++ book.rb
@@ -1,7 +1,13 @@
 class Book
   attr_accessor :title, :author
+    attr_reader :rentals
   def initialize(title,author)
     @title = tittle 
     @author = author
+    @rentals =[]
+  end
+
+  def add_rentals(rental)
+    @rentals.push(rental)
   end
 end
---
+++ rental.rb
@@ -0,0 +1,11 @@
+class Rental
+  attr_accessor :date, :book
+  def initialize(date)
+    @date = date
+  end
+
+  def book=(book)
+    @book = book
+    book.rentals.push(self) unless book.rentals.include?(self)
+  end
+end
