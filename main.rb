54ad33fe48543a29a873a5a0bc96e4a7be680af5
alhajideen <salifunurudeen834@gmail.com>
2023-01-02 19:21:36 +0000

    add the init.rb file

--- main.rb
+++ main.rb
@@ -2,28 +2,15 @@
 
 require './app'
 require './requests'
+require './init'
 
 class Main
   include Requests
+  include Init
   def initialize
     @inst = App.new
+    init
   end
-  def init
-    options = ['Please choose an option by entering a number',
-               '1 - List all books.', '2 - List all people.',
-               '3 - Create a person(1 - teacher or 2 - student).',
-               '4 - Create a book.', '5 - Create a rental.',
-               '6 - List all rentals for a given person id.',
-               '7 - Exit']
-    options.each do |opt|
-      puts opt
-    end
-    req = gets.chomp
-    request(req)
-  end
-
- 
-
 end
 
 obj = Main.new
