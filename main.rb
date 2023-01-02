ed9911b7c54615a5db79aeaac8898053abe3373c
alhajideen <salifunurudeen834@gmail.com>
2023-01-02 15:59:09 +0000

    add request method in Requests module

--- main.rb
+++ main.rb
@@ -21,30 +21,7 @@ class Main
     request(req)
   end
 
-  def request(request)
-    case request
-    when '1'
-      @inst.all_books
-      init
-    when '2'
-      @inst.all_people
-      init
-    when '3'
-      @inst.create_person
-      init
-    when '4'
-      book
-    when '5'
-      @inst.create_rental
-      init
-    when '6'
-      @inst.all_rentals
-      init
-    else
-      p 'Thanks for using this app, See you again'
-      exit
-    end
-  end
+ 
 
   def book
     p 'Enter title: '
